c96d244169deb6694f08b47d9ce7e47ab2ed7575
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-04 23:14:44 +0600

    added redirects file for netlify issue

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import CustomLink from '../CustomLink/CustomLink';
+import { Outlet } from 'react-router-dom';
 import CustomLink2 from '../CustomLink/CustomLink2';
 
 const Contact = () => {
