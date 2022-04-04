67d975b5dd8f5bf1347360b6d76c4b28052aaf89
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-04 19:04:01 +0600

    first question answer done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <article>
-                <h1></h1>
-                <p></p>
+        <div className="w-3/4 mx-auto">
+            <article className="mt-2">
+                <h1 className="text-5xl text-center">What is Context Api?</h1>
+                <p className="text-xl text-center">Conext API is a effective method for passing data from one component to another  component without props drilling. Context API helps us to create a global variable which can be pass easily. In easy words Context API acts like Wi-Fi.We can create a context and give it a range. Inside it's range anyone can use the context. It helps us from props drilling which is a very complex way to share data with components.</p>
             </article>
-            <article>
-                <h1></h1>
-                <p></p>
+            <article className="mt-2">
+                <h1 className="text-5xl text-center">What is Context Api?</h1>
+                <p className="text-xl text-center">Conext API is a effective method for passing data from one component to another  component without props drilling. Context API helps us to create a global variable which can be pass easily. In easy words Context API acts like Wi-Fi.We can create a context and give it a range. Inside it's range anyone can use the context. It helps us from props drilling which is a very complex way to share data with components.</p>
             </article>
-            <article>
-                <h1></h1>
-                <p></p>
+            <article className="mt-2">
+                <h1 className="text-5xl text-center">What is Context Api?</h1>
+                <p className="text-xl text-center">Conext API is a effective method for passing data from one component to another  component without props drilling. Context API helps us to create a global variable which can be pass easily. In easy words Context API acts like Wi-Fi.We can create a context and give it a range. Inside it's range anyone can use the context. It helps us from props drilling which is a very complex way to share data with components.</p>
             </article>
         </div>
     );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,20 +7,22 @@ import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
     const [click, setClick] = useState(false)
     return (
-        <div className="relative bg-gray-300 py-1 navbar">
-            <div className="md:hidden text-center bg-gray-300">
-                {
-                    click ? <FontAwesomeIcon icon={faBars} onClick={() => setClick(!click)} className="h-8 w-8 mx-auto cursor-pointer"></FontAwesomeIcon> : <FontAwesomeIcon icon={faXmark} onClick={() => setClick(!click)} className="h-8 w-8 mx-auto cursor-pointer"></FontAwesomeIcon>
-                }
+        <div className="sticky top-0 z-30">
+            <div className="relative bg-gray-300 py-1 navbar">
+                <div className="md:hidden text-center bg-gray-300">
+                    {
+                        click ? <FontAwesomeIcon icon={faBars} onClick={() => setClick(!click)} className="h-8 w-8 mx-auto cursor-pointer"></FontAwesomeIcon> : <FontAwesomeIcon icon={faXmark} onClick={() => setClick(!click)} className="h-8 w-8 mx-auto cursor-pointer"></FontAwesomeIcon>
+                    }
+                </div>
+                <nav className={`w-full flex justify-center flex-col md:flex-row absolute md:static bg-gray-300 ${click ? "-top-96" : "top-8"} z-10 duration-1000 pb-1`}>
+                    <CustomLink to='/' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">Home</CustomLink>
+                    <CustomLink to='reviews' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">Reviews</CustomLink>
+                    <CustomLink to='dashboard' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">Dashboard</CustomLink>
+                    <CustomLink to='blogs' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">Blogs</CustomLink>
+                    <CustomLink to='about' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">About</CustomLink>
+                    <CustomLink to='contact' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">Contact</CustomLink>
+                </nav>
             </div>
-            <nav className={`w-full flex justify-center flex-col md:flex-row absolute md:static bg-gray-300 ${click ? "-top-96" : "top-8"} z-10 duration-1000 pb-1`}>
-                <CustomLink to='/' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">Home</CustomLink>
-                <CustomLink to='reviews' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">Reviews</CustomLink>
-                <CustomLink to='dashboard' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">Dashboard</CustomLink>
-                <CustomLink to='blogs' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">Blogs</CustomLink>
-                <CustomLink to='about' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">About</CustomLink>
-                <CustomLink to='contact' className="text-xl ml-0 md:ml-5 p-3 rounded-md text-center">Contact</CustomLink>
-            </nav>
         </div>
     );
 };
